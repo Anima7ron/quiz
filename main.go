13e03a7343b5bc2623e21f027dfa22a1dddb4c98
Anima7ron <george@forzaesotico.com>
2020-07-25 18:51:27 -0500

    Added comments, removed watermark

--- main.go
+++ main.go
@@ -10,29 +10,33 @@ import (
 )
 
 func main() {
+	//Set flags for input CSV and time-limit
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
 	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 	flag.Parse()
-
+	//Open CSV file
 	file, err := os.Open(*csvFilename)
 	if err != nil {
 		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
 		os.Exit(1)
 	}
+	//Parse CSV
 	r := csv.NewReader(file)
 	lines, err := r.ReadAll()
 	if err != nil {
 		exit("Failed to parse the provided CSV file")
 	}
 	problems := parseLines(lines)
-
+	//Initialize timer and correct answer counter
 	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 	correct := 0
 
 problemloop:
 	for i, p := range problems {
+		//Present each problem
 		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
 		answerCh := make(chan string)
+		//Submit each answer
 		go func() {
 			var answer string
 			fmt.Scanf("%s\n", &answer)
@@ -40,18 +44,22 @@ problemloop:
 		}()
 
 		select {
+		//Timer runs out
 		case <-timer.C:
 			fmt.Println()
 			break problemloop
-
+		//Answer is submitted
 		case answer := <-answerCh:
 			if answer == p.a {
 				correct++
 			}
 		}
 	}
+	//Present Score
 	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
 }
+
+//Convert parsed problem to problem structure
 func parseLines(lines [][]string) []problem {
 	ret := make([]problem, len(lines))
 	for i, line := range lines {
@@ -72,13 +80,3 @@ func exit(msg string) {
 	fmt.Println(msg)
 	os.Exit(1)
 }
-
-/*
-
-  dBBBBBb     dBBBBb  dBP dBBBBBBb dBBBBBb dBBBBb dBBBBBb    dBBBBP dBBBBb
-       BB        dBP           dBP      BB            dBP   dBP.BP     dBP
-   dBP BB   dBP dBP dBP dBPdBPdBP   dBP BB   dBP  dBBBBK   dBP.BP dBP dBP
-  dBP  BB  dBP dBP dBP dBPdBPdBP   dBP  BB  dBP  dBP  BB  dBP.BP dBP dBP
- dBBBBBBB dBP dBP dBP dBPdBPdBP   dBBBBBBB dBP  dBP  dB' dBBBBP dBP dBP
-
-*/
